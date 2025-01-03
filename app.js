2a248419da005b0d0620cbdd6d83514c7ae49b42
ananijoel <ananijoellanatide@gmail.com>
2025-01-03 12:10:00 +0000

    modification du message de bienvenu

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app
     .use(bodyParser.json())
     .use(cors({ origin: 'http://127.0.0.1:5500' }))
 
-app.get('/', (req, res) => res.json('hello Todo_list_api'))
+app.get('/', (req, res) => res.json('hello Todo_list_restapi'))
 
 //todos routes
 require('./src/routes/todos/add-todo')(app)
